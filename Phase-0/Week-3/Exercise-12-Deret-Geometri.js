59812c9f85411c1b394f0223353267173e51a9fe
Edison <edirates@gmail.com>
2019-07-03 18:11:33 +0700

    Create Exercise-12-Deret-Geometri.js

---
+++ Phase-0/Week-3/Exercise-12-Deret-Geometri.js
@@ -0,0 +1,30 @@
+/* Phase 0 Week 3 Exercise 12 */
+/* Deret Geometri */
+
+function tentukanDeretGeometri(arr) {
+    var diff = 0;
+    var total = 0;
+    length = arr.length;
+    // Lakukan perulangan untuk mendapatkan total selisih bagi
+    for (var i = 0; i < length; i++) {
+        if (i !== length-1) {
+            diff = 0;
+            diff = parseInt(arr[i+1]) / parseInt(arr[i]);
+        }
+        total = total + diff;
+    }
+    // Hitung jumlah rata-rata selisih bagi
+    var average = total / (length);
+    // Apabila hasil rata-rata sama dengan selisih maka deret geometri
+    if (average === diff) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
