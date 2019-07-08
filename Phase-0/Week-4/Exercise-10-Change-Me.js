2e933b1ee03a233046c445e01d107d4504e8e57a
Edison <edirates@gmail.com>
2019-07-08 22:44:32 +0700

    Create Exercise-10-Change-Me.js

---
+++ Phase-0/Week-4/Exercise-10-Change-Me.js
@@ -0,0 +1,33 @@
+/* Phase 0 Week 4 Exercise 10 */
+/* Change Me */
+
+function changeMe(arr) {
+    var rowlength = arr.length;
+    for (var i = 0; i < arr.rowlength; i++) {
+        var collength = arr[i].length;
+        for (var j = 0; j < collength; j++) {
+            var thnLahir = arr[i][4];
+            var d = new Date();
+            if (thnLahir === "" || thnLahir === undefined) {
+
+            }
+            var arrObj = {
+
+            }
+        }
+    }
+    return arrObj;
+}
+
+changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); 
+// 1. Christ Evans:
+// { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 37 }
+// 2. Robert Downey:
+// { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+changeMe([]); // ""
