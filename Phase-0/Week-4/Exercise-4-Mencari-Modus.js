d2d30c92951831bcf03a71edc4f77e94b107fcdd
Edison <edirates@gmail.com>
2019-07-08 17:51:49 +0700

    Update Exercise-4-Mencari-Modus.js

--- Phase-0/Week-4/Exercise-4-Mencari-Modus.js
+++ Phase-0/Week-4/Exercise-4-Mencari-Modus.js
@@ -2,24 +2,40 @@
 /* Mencari Modus */
 
 function cariModus(arr) {
-    var modus = -1;
+    // Deklarasi variabel modus untuk menampung nilai modus yang ditemukan
+    var modus = 0;
+    // Deklarasi variabel most untuk menampung jumlah char yang paling banyak muncul
     var most = 1;
+    // Deklarasi variabel counter untuk menghitung jumlah char yang muncul saat ini
     var counter = 0;
+    // Deklarasi variabel untuk menghitung total counter yang ditemukan
+    var total = 0;
+    // Lakukan nested for untuk mencari jumlah char yang muncul
     var length = arr.length;
     for (var i = 0; i < length; i++) {
         counter = 0;
         for (var j = 0; j < length; j++) {
+            // Jika char saat ini sama dengan char yang dicari maka tambah counter
             if (arr[i] === arr[j]) {
                 counter++;
             }
         }
+        // Apabila sudah selesai mencari selanjutnya menentukan nilai modus
+        // Jika isi dari array semua nilai yang sama maka set modus = -1
         if (counter === length) {
             modus = -1;
         } 
+        // Jika nilai counter lebih besar daripada most saat ini maka gantikan most dengan counter dan set modus dengan char saat ini
         else if (counter > most) {
             most = counter;
             modus = arr[i];
         }
+        // Tambahkan counter yang ditemukan ke dalam variabel total
+        total = total + counter;
+    }
+    // Jika rata-rata counter yang ditemukan sama dengan jumlah counter paling banyak ditemukan maka bukan modus
+    if ( (total/length) === most ) {
+        modus = -1;
     }
     return modus;
 }
