b7207660acee58bf2538ee09e0b5410224978910
Edison <edirates@gmail.com>
2019-07-09 11:18:10 +0700

    Update Exercise-10-Change-Me.js

--- Phase-0/Week-4/Exercise-10-Change-Me.js
+++ Phase-0/Week-4/Exercise-10-Change-Me.js
@@ -2,21 +2,37 @@
 /* Change Me */
 
 function changeMe(arr) {
-    var rowlength = arr.length;
-    for (var i = 0; i < arr.rowlength; i++) {
-        var collength = arr[i].length;
-        for (var j = 0; j < collength; j++) {
-            var thnLahir = arr[i][4];
-            var d = new Date();
-            if (thnLahir === "" || thnLahir === undefined) {
-
-            }
-            var arrObj = {
-
-            }
+    // Deklarasi awal variabel object literal
+    var arrObj = {
+        firstName: "",
+        lastName: "",
+        gender: "",
+        age: ""
+    };
+    // Cari tanggal sekarang dan ambil tahun nya saja untuk menghitung umur
+    var tglSkrg = new Date();
+    var thnSkrg = tglSkrg.getFullYear(); 
+    var length = arr.length;
+    // Lakukan perulangan untuk menampilkan masing-masing nilai object dari array
+    for (var i = 0; i < arr.length; i++) {
+        // Set object class sesuai dengan nilai yang diberikan pada array
+        arrObj.firstName = arr[i][0];
+        arrObj.lastName = arr[i][1];
+        arrObj.gender = arr[i][2];
+        var thnLahir = arr[i][3];
+        // Jika tahun lahir kosong atau tidak ditemukan atau tahun lahir lebih besar dari tahun sekarang maka set invalid year
+        if (thnLahir === "" || thnLahir === undefined || thnLahir > thnSkrg) {
+            arrObj.age = "Invalid Birth Year";
+        } 
+        // Jika tidak maka hitung umur
+        else {
+            arrObj.age = thnSkrg - thnLahir;
         }
+        // Tampilkan ke monitor dengan format sebagai berikut
+        var string = (i + 1) + ". " + arr[i][0] + " " + arr[i][1] + ":";
+        console.log(string);
+        console.log(arrObj);
     }
-    return arrObj;
 }
 
 changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); 
