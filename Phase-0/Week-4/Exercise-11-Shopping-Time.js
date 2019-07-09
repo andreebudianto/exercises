04a3ecf00e0753edb27fa2d2255bcb44005cc7f5
Edison <edirates@gmail.com>
2019-07-09 12:07:24 +0700

    Update Exercise-11-Shopping-Time.js

--- Phase-0/Week-4/Exercise-11-Shopping-Time.js
+++ Phase-0/Week-4/Exercise-11-Shopping-Time.js
@@ -2,12 +2,14 @@
 /* Shopping Time */
 
 function shoppingTime(memberId, money) {
+    // Deklarasi variabel object untuk member
     var memberObj = {
         memberId: memberId,
         money: money,
         listPurchased: [],
         changeMoney: 0
     };
+    // Deklarasi variabel object untuk product
     var productObj = {
         productName: ["Sepatu Stacattu", 
                       "Baju Zoro", 
@@ -22,19 +24,30 @@ function shoppingTime(memberId, money) {
                        50000
                     ]
     };
+    // Jika member ID tidak diisi atau kosong maka tampilkan pesan error
     if (memberObj.memberId === "" || memberObj.memberId === undefined) {
         return "Mohon maaf, toko X hanya berlaku untuk member saja";
-    } else if (memberObj.money < 50000) {
+    } 
+    // Jika uang member dibawah 50rb maka tampilkan pesan error
+    else if (memberObj.money < 50000) {
         return "Mohon maaf, uang tidak cukup";
-    } else {
+    }
+    // Jika semua memenuhi maka lakukan perhitungan 
+    else {
+        // Lakukan perulangan sebanyak jumlah product yang bisa dibeli
         var productLength = productObj.productName.length;
         for (var i = 0; i < productLength; i++) {
+            // Jika uang member diatas harga product termahal
             if (memberObj.money >= productObj.productPrice[i]) {
+                // Masukkan nama product ke array listPurchased
                 memberObj.listPurchased.push(productObj.productName[i]);
+                // Kurangi uang member sekarang dengan harga product saat ini
                 memberObj.money = memberObj.money - productObj.productPrice[i];
+                // Masukkan sisa uang member saat ini
                 memberObj.changeMoney = memberObj.money;
             }
         }
+        // Set ulang uang member ke kondisi semula
         memberObj.money = money;
         return memberObj;
     }
