02fb62da5803b6e945002a602db94dcd84fbbc28
Edison <edirates@gmail.com>
2019-07-10 15:29:54 +0700

    Create Exercise-4-Total-Digit-Rekursif.js

---
+++ Phase-0/Week-5/Exercise-4-Total-Digit-Rekursif.js
@@ -0,0 +1,43 @@
+/* Phase 0 Week 5 Exercise 4 */
+/* Total Digit Rekursif */
+
+function totalDigitRekursif(angka) {
+    // Selama angka diatas nol maka lakukan proses rekursif
+    if (angka > 0) {
+        // Pertama-tama harus ubah dulu format angka menjadi string agar dapat dipotong
+        var str = angka.toString();
+        // Ambil digit pertama dan konversi ke dalam bentuk angka untuk ditambahkan pada hasil akhir
+        var strAwal = str.slice(0,1);
+        var angkaAwal = parseInt(strAwal);
+        // Ambil sisa digit angka setelahnya untuk dapat dijadikan parameter rekursif berikutnya
+        var strAkhir = str.slice(1);
+        var angkaAkhir = parseInt(strAkhir);
+        // Hitung total digit angka pertama
+        return angkaAwal + totalDigitRekursif(angkaAkhir);
+    }
+    // Jika sudah tidak ada angka lagi maka stop rekursif
+    else {
+        return 0;
+    }
+    // --------------- Method Split Array ---------------
+    // if (angka > 0) {
+    //     var str = angka.toString();
+    //     var arr = str.split("");
+    //     var arrAngka = arr[0];
+    //     var angkaAwal = parseInt(arrAngka);
+    //     arr.shift();
+    //     var arrJoin = arr.join("");
+    //     var newAngka = parseInt(arrJoin);
+    //     return angkaAwal + totalDigitRekursif(newAngka);
+    // }
+    // else {
+    //     return 0;
+    // }
+} 
+
+
+console.log(totalDigitRekursif(512)); // 8
+console.log(totalDigitRekursif(1542)); // 12
+console.log(totalDigitRekursif(5)); // 5
+console.log(totalDigitRekursif(21)); // 3
+console.log(totalDigitRekursif(11111)); // 5
