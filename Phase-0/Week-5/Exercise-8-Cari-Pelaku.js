5e95d380756fd667a99407c8583b19d39be4e91b
Edison <edirates@gmail.com>
2019-07-10 22:13:37 +0700

    Create Exercise-8-Cari-Pelaku.js

---
+++ Phase-0/Week-5/Exercise-8-Cari-Pelaku.js
@@ -0,0 +1,13 @@
+/* Phase 0 Week 5 Exercise 8 */
+/* Cari Pelaku */
+
+function cariPelaku(str) {
+    return str.match(/abc/gi).length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
