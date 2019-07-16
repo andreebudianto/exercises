1f3f69f49290876cdc3d6112bf3b9355b9a71f28
Edison <edirates@gmail.com>
2019-07-16 14:10:16 +0700

    Update script.js

--- Phase-0/Week-6/Latihan-Pribadi-1/script.js
+++ Phase-0/Week-6/Latihan-Pribadi-1/script.js
@@ -1,24 +1,19 @@
 // Pertama ambil tag body
-// var body = document.body;
+var body = document.body;
 
 // Ambil children dari tag body dengan nilai id
-// var divContainer = document.getElementById("container");
+var divContainer = document.getElementById("container");
 
 // Ambil children dari div container dengan nilai id
 var h1Container = document.getElementById("fill-me");
-h1Container.innerHTMl = "HALO!";
+h1Container.innerHTML = "HALO!";
 
 // Ambil children dari div container dengan nilai class
 var classParagraph = document.getElementsByClassName("change-all-of-me");
 for (var i = 0; i < classParagraph.length; i++) {
-    classParagraph[i].innerHTMl = "HALO JUGA!";
+    classParagraph[i].innerHTML = "HALO JUGA!";
 }
 
 // Ambil children dari div container dengan nilai tag
 var h2Container = document.getElementsByTagName("h2");
-h2Container.innerHTMl = "Apa Kabar!";
-
-
-
-
-
+h2Container[0].innerHTML = "Apa Kabar!";
